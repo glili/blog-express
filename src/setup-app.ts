6e0963621d5d7ca17aae008208322909bd094be7
Lili <you@example.com>
2025-05-19 16:28:40 -0400

    typo

--- src/setup-app.ts
+++ src/setup-app.ts
@@ -2,7 +2,7 @@ import express, { Express } from 'express';
 import { BLOGS_PATH, POSTS_PATH, TESTING_PATH } from './core/paths/paths';
 import { blogsRouter } from './blogs/routers/blogs.router';
 import { postsRouter } from './posts/routers/posts.router';
-import { testingRouter } from './testing/routers/testing.routrt';
+import { testingRouter } from './testing/routers/testing.router';
 
 export const setupApp = (app: Express) => {
   app.use(express.json());
