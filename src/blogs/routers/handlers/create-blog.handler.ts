584176e11acd1788e242c63041187743f604205f
Lili <you@example.com>
2025-05-20 22:37:09 -0400

    id str

--- src/blogs/routers/handlers/create-blog.handler.ts
+++ src/blogs/routers/handlers/create-blog.handler.ts
@@ -6,16 +6,18 @@ import { Blog } from '../../types/blog';
 import { blogsRepository } from '../../repositories/blogs.repository';
 
 export function createBlogHandler(
-  req: Request<{}, {}, BlogInput>,
-  res: Response,
+    req: Request<{}, {}, BlogInput>,
+    res: Response,
 ) {
+  const lastId = db.blogs.length > 0 ? Number(db.blogs[db.blogs.length - 1].id) : 0;
+
   const newBlog: Blog = {
-    id: db.blogs.length ? db.blogs[db.blogs.length - 1].id + 1 : 1,
+    id: String(lastId + 1), // ✅ ensure id is string
     name: req.body.name,
     description: req.body.description,
     websiteUrl: req.body.websiteUrl,
   };
 
   blogsRepository.create(newBlog);
-  res.status(HttpStatus.Created).send(newBlog);
+  res.status(HttpStatus.Created).send(newBlog); // ✅ correct response
 }
