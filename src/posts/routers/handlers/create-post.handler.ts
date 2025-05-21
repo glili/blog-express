f9133b109b56a3805479b5999227e8a7936bef5f
Lili <you@example.com>
2025-05-21 00:11:11 -0400

    post

--- src/posts/routers/handlers/create-post.handler.ts
+++ src/posts/routers/handlers/create-post.handler.ts
@@ -5,18 +5,33 @@ import { db } from '../../../db/in-memory.db';
 import { Post } from '../../types/post';
 import { postsRepository } from '../../repositories/posts.repository';
 import { PostCreateInput } from '../../dto/post-create.input';
+import {blogsRepository} from "../../../blogs/repositories/blogs.repository";
+import {createErrorMessages} from "../../../core/utils/error.utils";
 
 export function createPostHandler(
   req: Request<{}, {}, PostCreateInput>,
   res: Response,
 ) {
+  const blogId = req.body.blogId;
+
+  const blog = blogsRepository.findById(blogId);
+
+  if (!blog) {
+    res
+        .status(HttpStatus.BadRequest)
+        .send(
+            createErrorMessages([{ field: 'id', message: 'Driver not found' }]),
+        );
+
+    return;
+  }
   const newPost: Post = {
     id: String(db.posts.length ? db.posts[db.posts.length - 1].id + 1 : 1),
     title: req.body.title,
     shortDescription: req.body.shortDescription,
     content: req.body.content,
     blogId: req.body.blogId,
-    blogName: blog.blogName,
+    blogName: blog.name,
   };
 
   postsRepository.create(newPost);
